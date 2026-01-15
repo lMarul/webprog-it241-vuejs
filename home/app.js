a85532fd352debf3304446139c9847151b57cec0
Maru <mggonzales@student.apc.edu.ph>
2026-01-15 21:23:17 +0800

    using images.js

--- home/app.js
+++ home/app.js
@@ -11,9 +11,16 @@ createApp({
       section5: '5 Hobbies & Interests',
       section6: '6 Goals In Life / Dream',
       section7: '7 Picture Gallery',
-      footer: 'Asia Pacific College'
+      footer: 'Asia Pacific College',
+      manyFoods: [
+        'https://www.w3schools.com/vue/img_burrito.svg',
+        'https://www.w3schools.com/vue/img_salad.svg',
+        'https://www.w3schools.com/vue/img_cake.svg',
+        'https://www.w3schools.com/vue/img_soup.svg',
+        'https://www.w3schools.com/vue/img_fish.svg',
+        'https://www.w3schools.com/vue/img_pizza.svg',
+        'https://www.w3schools.com/vue/img_rice.svg'
+      ]
     };
   }
 }).mount('#app');
-
-// I don't know what I am doing anymore
